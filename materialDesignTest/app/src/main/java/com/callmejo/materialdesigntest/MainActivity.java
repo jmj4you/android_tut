9fa198b4e97cff5ade8afca8ceb6d1e522f7b5a1
JOSEPH M J <josephmj@hifx.net>
2015-05-14 18:31:05 +0530

    #3 Android ToolBar Example:

--- materialDesignTest/app/src/main/java/com/callmejo/materialdesigntest/MainActivity.java
+++ materialDesignTest/app/src/main/java/com/callmejo/materialdesigntest/MainActivity.java
@@ -4,17 +4,26 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar; //
 import android.view.Menu;
 import android.view.MenuItem;
+//import android.widget.Toolbar;
+
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
