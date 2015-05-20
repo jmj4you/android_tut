6d50d8edd1e7acbb680ca030a642d3c5462a8e74
JOSEPH M J <josephmj@hifx.net>
2015-05-20 11:32:37 +0530

    webservice basic

--- Webservice/app/src/main/java/com/example/jomon/webservice/MainActivity.java
+++ Webservice/app/src/main/java/com/example/jomon/webservice/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.jomon.webservice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -13,19 +15,23 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends Activity {
 
-    final TextView mTextView = (TextView) findViewById(R.id.text);
-
+    TextView usernameTextView, fullnameTextView, errorTextView;
+    String url = "http://graph.facebook.com/jmj4you";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        usernameTextView = (TextView) findViewById(R.id.username);
+        fullnameTextView = (TextView) findViewById(R.id.fullname);
+        errorTextView = (TextView) findViewById(R.id.error);
     }
 
     @Override
@@ -49,28 +55,44 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void getMyaddress(){
+
+    public void getMyaddress(View view) {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://www.google.com";
+
 
 // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: " + response.substring(0, 100));
+                        JSONObject obj = myJSON(response);
+                        fullnameTextView.setText("Name: " + obj.optString("name"));
+                        usernameTextView.setText("Username: " + obj.optString("username"));
+
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
+//                BreakIterator errorTextView;
+                errorTextView.setText("That didn't work!");
             }
         });
 // Add the request to the RequestQueue.
         queue.add(stringRequest);
 
 
+    }
+
+    /*JSON paarse customize */
+    public JSONObject myJSON(String response) {
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(response);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return obj;
     }
 }
---
+++ Webservice/readme.md
@@ -0,0 +1,80 @@
+
+#These lines should be noted
+AndroidManifest.xml
+
+<pre>
+    <uses-permission android:name="android.permission.INTERNET" />
+</pre>
+
+#You have to add library for volley
+ 1. Download volley.jar from
+ 2. Put it into app/libs
+ 3. Add dependencies *** compile files('libs/volley.jar') *** into build.gradle(Mobule.app)
+ 4. Clean and rebuild app
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+MainActivity.java
+
+
+<pre>
+  TextView usernameTextView,fullnameTextView,errorTextView;
+    String url = "http://graph.facebook.com/jmj4you";
+  .....
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        usernameTextView = (TextView) findViewById(R.id.username);
+        fullnameTextView = (TextView) findViewById(R.id.fullname);
+        errorTextView = (TextView) findViewById(R.id.error);
+    }
+
+   public void getMyaddress(View view) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject obj = myJSON(response);
+                        fullnameTextView.setText("Name: "+ obj.optString("name"));
+                        usernameTextView.setText("Username: "+ obj.optString("username"));
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                BreakIterator errorTextView;
+                errorTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
+    }
+
+    /*JSON paarse customize */
+    public JSONObject myJSON(String response) {
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(response);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return obj;
+    }
+
+</pre>
+
+
+->ActionBar
+https://developer.android.com/guide/topics/ui/actionbar.html
