2835222ce6fc2b9743e863a9e83abc46669e76e5
jomon <jmjonly4you@gmail.com>
2015-05-22 07:49:24 +0530

    gmap with current location started

---
+++ GmapWithCurrentLocation/app/src/main/java/com/example/jomon/gmapwithcurrentlocation/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.jomon.gmapwithcurrentlocation;
+
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    TextView textLat, textLong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        textLat = (TextView) findViewById(R.id.textLat);
+//        textLong = (TextView) findViewById(R.id.textLong);
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new MyLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+    }
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null) {
+
+                double plat = location.getLatitude();
+                double plong = location.getLongitude();
+                Toast.makeText(getApplicationContext(),"Lat :"+plat+" Long: "+plong,Toast.LENGTH_SHORT);
+//                textLat.setText(Double.toString(plat));
+//                textLong.setText(Double.toString(plong));
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+}
