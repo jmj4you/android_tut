1cc21b49a5fb8bd944c06087da8524d3232d76aa
JOSEPH M J <josephmj@hifx.net>
2015-05-29 10:59:57 +0530

    Actual use for error display

--- HttpApplication/app/src/main/java/in/jmj/webservice/Api.java
+++ HttpApplication/app/src/main/java/in/jmj/webservice/Api.java
@@ -1,38 +1,24 @@
 package in.jmj.webservice;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.callmejo.httpapplication.ApplicationController;
 import com.callmejo.httpapplication.MainActivity;
+import com.callmejo.httpapplication.R;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by jomon on 26/5/15.
  */
-public class Api {
+public class Api  {
     public static String URL_TEST_BASE = "http://jomon-mybc.rhcloud.com/";
     public static String URL_BASE = "http://jomon-mybc.rhcloud.com/api/v1";
     public static String URL_BLOOD_GROUPS = URL_BASE + "/bloodgroups";
@@ -58,7 +44,8 @@ public class Api {
         return obj;
     }
 
-    public JSONObject parseMyResponse(String response) {
+    public JSONObject parseMyResponse(Context context, String response) {
+
 
         JSONObject obj = toJSON(response);
 
